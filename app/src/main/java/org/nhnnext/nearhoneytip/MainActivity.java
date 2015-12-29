a39796f0f99edb74a70b374e9f289367e792ede0
luvgaram <garam@nhnnext.org>
2015-12-29 10:53:17 +0900

    [location] change to gms

--- app/src/main/java/org/nhnnext/nearhoneytip/MainActivity.java
+++ app/src/main/java/org/nhnnext/nearhoneytip/MainActivity.java
@@ -2,15 +2,12 @@ package org.nhnnext.nearhoneytip;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
@@ -19,7 +16,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
+
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -31,6 +28,12 @@ import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 
 import org.nhnnext.nearhoneytip.adapter.TipListAdapter;
 import org.nhnnext.nearhoneytip.item.TipItem;
@@ -45,7 +48,9 @@ import retrofit.Callback;
 import retrofit.RetrofitError;
 import retrofit.client.Response;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     private TipListAdapter tipListAdapter;
     private List<TipItem> tipItems;
@@ -53,6 +58,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private String nickname;
     private String profilephoto;
     private SharedPreferences pref;
+    private LocationRequest locationRequest;
+    private GoogleApiClient googleApiClient;
 
     public final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
     public final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 2;
@@ -65,7 +72,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         setContentView(R.layout.activity_main_drawer);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        pref = getSharedPreferences("gps", MODE_PRIVATE);
+//        pref = getSharedPreferences("gps", MODE_PRIVATE);
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
@@ -81,23 +88,39 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             getGPSPermission();
         }
 
-        setFAB();
-        setDrawer(toolbar);
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(100);
+        locationRequest.setFastestInterval(20);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        if (googleApiClient == null) {
+
+            Log.d("location", "setGoogleApiClient");
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
             isGPSOk = true;
+//            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+//
+//            if (location == null) {
+//                Log.d("location", "null");
+//            }
+//            else Log.d("location", location.getLatitude() + " : " + location.getLongitude());
 
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-            if (location == null) {
-                Log.d("location", "null");
-            }
-            else Log.d("location", location.getLatitude() + " : " + location.getLongitude());
         } else {
             getGPSPermission();
         }
+
+        setFAB();
+        setDrawer(toolbar);
     }
 
     private void setFAB() {
@@ -213,10 +236,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setTipList();
+    protected void onStart() {
+        googleApiClient.connect();
+        super.onStart();
     }
 
     private void setTipList() {
@@ -271,12 +293,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-
-    }
-
-    @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
@@ -293,6 +309,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        Log.d("location", "onConnected");
+        if (ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                return;
+
+        Log.d("location", "Request fusedLocation");
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.d("location", "lat: " + location.getLatitude() + "long: " + location.getLongitude());
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
 
 //    public void showSettingAlert(){
 //        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
@@ -326,27 +381,27 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 //        editor.commit();
 //    }
 
-    LocationListener locationListener = new LocationListener() {
-        public void onLocationChanged(Location location) {
-            double latitude = location.getLatitude();
-            double longitude = location.getLongitude();
-
-            Log.d("location", "lat: " + latitude + "long: " + longitude);
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    };
+//    LocationListener locationListener = new LocationListener() {
+//        public void onLocationChanged(Location location) {
+//            double latitude = location.getLatitude();
+//            double longitude = location.getLongitude();
+//
+//            Log.d("location", "lat: " + latitude + "long: " + longitude);
+//        }
+//
+//        @Override
+//        public void onStatusChanged(String provider, int status, Bundle extras) {
+//
+//        }
+//
+//        @Override
+//        public void onProviderEnabled(String provider) {
+//
+//        }
+//
+//        @Override
+//        public void onProviderDisabled(String provider) {
+//
+//        }
+//    };
 }
